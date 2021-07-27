7c0d2c91f285f1890ddcbe08634a480e58bee6cb
Aditya Kumar <adikumar209@gmail.com>
2021-07-28 02:09:53 +0530

    prod ready

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,37 @@ import '../styles/globals.css'
 import 'antd/dist/antd.css'; 
 import React from 'react'
 import { wrapper } from "../redux/store"
-import Root from './_root';
+import Cookies from 'js-cookie';
+import {connect} from 'react-redux'
+import * as actionTypes from '../redux/actions/main'
 
-function MyApp({Component, pageProps}) {
+function MyApp(props) {
+  const {Component, pageProps}=props
+  React.useEffect(()=>{
+    if(!props.loggedIn){
+        const val=Cookies.get('JWT')
+        if (val){
+            props.login(true)
+        }
+    }
+},[props])
 
-  return <Root pageProps={pageProps} Component={Component} />
+const Layout = Component?.Layout ?? React.Fragment;
+console.log(props)
+return (
+<Layout>
+<Component {...props.pageProps} loggedIn={props.loggedIn}/>
+</Layout>
+)
   
 }
 
+const mapStateToProps=state=>({
+  loggedIn:state.main.loggedIn
+})
 
-export default wrapper.withRedux(MyApp)
+const mapDispatchToProps=dispatch=>({
+  login:(status)=>dispatch(actionTypes.setToken(status))
+})
+
+export default wrapper.withRedux(connect(mapStateToProps,mapDispatchToProps)(MyApp))
